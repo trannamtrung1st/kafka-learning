146319e1e64ec58bc1a3b29b0a0beadf86176c94
trung-tran-sts <trung.tran@saigontechnology.com>
2022-10-01 21:53:57 +0700

    Add rack awareness

--- TStore/misc/kafka/scripts/start.sh
+++ TStore/misc/kafka/scripts/start.sh
@@ -4,9 +4,11 @@ cp ./config-override/zookeeper.properties ./config/zookeeper.properties;
 mkdir /zookeeper-data
 echo "$BROKER_ID" > /zookeeper-data/myid
 
-sed -i "s/broker.id=1/broker.id=$BROKER_ID/g" ./config/server.properties;
-sed -i "s/:9093/:$LOCAL_PORT/g" ./config/server.properties;
-sed -i "s/:29093/:$DOCKER_PORT/g" ./config/server.properties;
+sed -i -e "s/{BROKER_ID}/$BROKER_ID/g" \
+	-e "s/{BROKER_RACK}/$BROKER_RACK/g" \
+	-e "s/:{LOCAL_PORT}/:$LOCAL_PORT/g" \
+	-e "s/:{DOCKER_PORT}/:$DOCKER_PORT/g" \
+	./config/server.properties;
 
 bin/zookeeper-server-start.sh ./config/zookeeper.properties &
 $CREATE_ACLS = "true" && scripts/create-default-acls.sh &
